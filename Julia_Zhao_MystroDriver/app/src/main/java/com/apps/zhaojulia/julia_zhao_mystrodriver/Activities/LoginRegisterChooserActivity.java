376e0b8f1562e5cc389c2b235331af4369183648
Julia Zhao <zjulia@vt.edu>
2017-09-18 17:57:56 -0700

    Init

---
+++ Julia_Zhao_MystroDriver/app/src/main/java/com/apps/zhaojulia/julia_zhao_mystrodriver/Activities/LoginRegisterChooserActivity.java
@@ -0,0 +1,15 @@
+package com.apps.zhaojulia.julia_zhao_mystrodriver.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.zhaojulia.julia_zhao_mystrodriver.R;
+
+public class LoginRegisterChooserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register_chooser);
+    }
+}
---
+++ Julia_Zhao_MystroDriver/app/src/main/java/com/apps/zhaojulia/julia_zhao_mystrodriver/Model/User.java
@@ -0,0 +1,15 @@
+package com.apps.zhaojulia.julia_zhao_mystrodriver.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by admin on 9/18/17.
+ */
+
+public class User {
+
+    @SerializedName("email")
+    private String email;
+
+
+}
